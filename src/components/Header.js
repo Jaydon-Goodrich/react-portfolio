62f501484d8d4f8b9f6053653d95d0e933e6356c
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-12-19 01:21:42 -0700

    nav

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Header(){
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><h1>Jaydon Goodrich</h1></li>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="contact">Contact</a></li>
+                    <li><a href="resume">Resume</a></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
